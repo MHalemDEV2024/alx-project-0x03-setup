b93fa329272b27c0a2e4653c25b17e2335004613
Mohamed Abdelhlim Saeed <MHalem_DEV@outlook.com>
2025-07-27 17:58:26 +0300

    Implementing a share layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layouts/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to Splash App</h1>
+      {/* Your page content */}
+    </Layout>
+  )
+}
